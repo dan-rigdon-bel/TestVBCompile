5b19952516c4b5f4b9569498d34b8728d3ec9b63
Dan Belcher <dan@mcneel.com>
2018-12-06 10:05:49 -0800

    Initial commit. https://github.com/mono/mono/issues/11939

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,27 @@
+
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TestVBCompile")> 
+<Assembly: AssemblyDescription("A sample to show mono-sgen process never dies")> 
+<Assembly: AssemblyCompany("Robert McNeel & Associates")> 
+<Assembly: AssemblyProduct("TestVBCompile")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c7691111-5523-42dd-97b8-ba9117f3494b")> 
+
+' DO NOT DEFINE VERSION INFORMATION HERE
+' AssemblyVersion is defined in ..\..\..\..\version.vb
+' <Assembly: AssemblyVersion("0.0.0.0")> 
+' AssemblyFileVersion is defined in ..\..\..\..\version.vb
+' <Assembly: AssemblyFileVersion("0.0.0.0")> 
---
+++ Program.vb
@@ -0,0 +1,8 @@
+﻿Imports System
+
+Module Program
+  Sub Main (args As String())
+    Console.WriteLine("Test app to demonstrate that mono-sgen progress never dies")
+  End Sub
+End Module
+ 
